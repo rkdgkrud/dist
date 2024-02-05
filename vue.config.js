d1698a7f9173a1c2bbcf7088d5f47ceaf29ae120
강하경 <rkdgkrud3@naver.com>
2024-02-05 20:44:02 +0900

    CI TEST

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: 'https://github.com/rkdgkrud/vue/',
+  publicPath: '/vue/',
   outputDir: 'dist' ,
   transpileDependencies: true,
 
