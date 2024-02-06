98d839f1d16fccc13bf0908f6cc9ff03db1dab5f
강하경 <rkdgkrud3@naver.com>
2024-02-06 23:29:46 +0900

    test

--- after-deploy.sh
+++ after-deploy.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-REPOSITORY=/home/ubuntu/build
+# Set NVM_DIR
+export NVM_DIR="$HOME/.nvm"
 
+# Load NVM
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Load bash_profile
+source ~/.bash_profile
+
+# Change directory to your repository
+REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
+
+# Install Node.js 14
 nvm install 14
 nvm use 14
 
+# Install dependencies with yarn
 yarn
+
+# Start the application with pm2
 pm2 start dist
